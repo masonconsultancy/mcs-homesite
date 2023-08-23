959dc6d6284257f680b997981cd536c37dd93946
Gary John Mason <gary.mason@mason-consultancy.com>
2023-08-23 11:29:00 +0100

    Updated the look and feel of the UI

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ namespace mcs_homesite.Pages
 {
     public class IndexModel : PageModel
     {
+        public readonly string ChatMessageText = "Hello I found your site";
+        public readonly string ChatMessageNumber = "+447847337097";
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
