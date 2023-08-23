be981a027a9438ea43965aac817855e060e78621
Gary John Mason <gary.mason@mason-consultancy.com>
2023-08-23 11:40:46 +0100

    Added a demo page

---
+++ Pages/Demo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mcs_homesite.Pages
+{
+    public class DemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
